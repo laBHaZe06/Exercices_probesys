e66610177954c3d301dd33e48055d6edc7b8fdcb
laBHaZe06 <borne.yoan@outlook.fr>
2025-03-22 12:28:54 +0100

    update

--- app/app/models/player.rb
+++ app/app/models/player.rb
@@ -4,6 +4,7 @@ class Player < ApplicationRecord
   validates :position, presence: true, inclusion: { in: ["Heal", "Tank", "DPS"] }
   validates :team_id, presence: true
   validates :max_players_team, presence: true
+  
   def max_players_team
     team = Team.find(team_id)
     if team.players.count >= 11
